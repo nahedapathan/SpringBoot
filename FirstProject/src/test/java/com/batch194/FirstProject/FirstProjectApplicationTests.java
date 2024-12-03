4b80d486cc2045e92dfe0be3dc7e93f8f3238f16
Pathan Naheda Mohammed khan <99787055+nahedapathan@users.noreply.github.com>
2024-12-03 17:27:05 +0530

    Add files via upload

---
+++ FirstProject/src/test/java/com/batch194/FirstProject/FirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batch194.FirstProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
